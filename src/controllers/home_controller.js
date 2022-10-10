36b413e0b6b9781a7c4ab41cff7a2754ef394662
Paulo Lopes <paulogean2@gmail.com>
2022-10-10 14:10:52 -0400

    add nodemon

--- src/controllers/home_controller.js
+++ src/controllers/home_controller.js
@@ -1,6 +1,6 @@
 module.exports = {
     index:function (res, req) {
-        req.send('Curso de fullstack');
+        req.send('Curso de fullstack::');
     },
     sobre: (res, req) => {
         req.send({ app: "Academia backend" ,versao:"1.0"})
